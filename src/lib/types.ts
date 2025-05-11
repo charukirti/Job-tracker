d91fe50f8c2754fe5bd6734d18f6952860edbb87
charukirti <charukirticc112002@gmail.com>
2025-05-11 23:19:10 +0530

    feat: types for application

---
+++ src/lib/types.ts
@@ -0,0 +1,23 @@
+import { $Enums } from "@prisma/client";
+
+export type Application = {
+  id: string;
+  company: string;
+  jobTitle: string;
+  status: $Enums.Status;
+  dateApplied: Date;
+  notes: string | null;
+  jobUrl: string | null;
+  location: string | null;
+  remote: boolean | null;
+  salaryRange: string | null;
+  nextInterviewDate: Date | null;
+  interviewStage: $Enums.InterviewStage | null;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export interface TableLayoutProp {
+  applications: Application[];
+}
