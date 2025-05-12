bab928c0ae838bf7c6d7e57054c412a19b06a31c
charukirti <charukirticc112002@gmail.com>
2025-05-12 17:12:23 +0530

    feat: Add application button to redirect user on form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,21 @@
 import { getUserApplications } from "@/actions/applications";
 import TableLayout from "./components/TableLayout";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Applications() {
   const applications = await getUserApplications();
   return (
-    <section>
-      <h1>Applications Dashboard</h1>
+    <section className="mt-5">
+      <header className="flex items-center justify-between mb-5">
+        <div>
+          <h1 className="text-3xl font-bold">Applications Dashboard</h1>
+          <p className="text-lg">All your applied jobs will be visible here</p>
+        </div>
+        <Button>
+          <Link href={"/applications/new"}>Add New Application</Link>
+        </Button>
+      </header>
 
       <TableLayout applications={applications} />
     </section>
